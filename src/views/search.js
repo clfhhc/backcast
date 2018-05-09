7f0c89e5662741d779a9274cb1263c41b051d725
clfhhc <clfhhc@gmail.com>
2018-05-08 20:06:01 -0700

    Enter key in search works.

--- src/views/search.js
+++ src/views/search.js
@@ -1,7 +1,8 @@
 var SearchView = Backbone.View.extend({
 
   events: {
-    'click button': 'handleSearch'
+    'click button': 'handleSearch',
+    'keypress': 'keyAction'
   },
 
   render: function() {
@@ -12,6 +13,12 @@ var SearchView = Backbone.View.extend({
   handleSearch: function() {
     let query = $('.form-control').val();
     this.trigger('search', query);
+  },
+  
+  keyAction: function(e) {
+    if (e.which === 13) {
+      this.handleSearch();
+    }
   },
 
   template: templateURL('src/templates/search.html')
